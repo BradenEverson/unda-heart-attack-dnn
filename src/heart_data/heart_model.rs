e4739e9e275fb6d662ad30213994da1cd511eefc
Braden Everson <bradeneverson@gmail.com>
2024-04-14 15:56:54 -0500

    Train model

--- src/heart_data/heart_model.rs
+++ src/heart_data/heart_model.rs
@@ -1,3 +1,8 @@
+use std::error::Error;
+
+use unda::core::data::input::Input;
+use unda::util::csv_parser::CSVParse;
+
 pub struct HeartModel {
     age: f32,
     sex: f32,
@@ -21,5 +26,23 @@ impl HeartModel {
             ], 
             vec![self.res]
         )
+    }
+    pub fn from_file(path: &str) -> Result<(Vec<Vec<f32>>, Vec<Vec<f32>>), Box<dyn Error>> {
+        let mut float_parsed: Vec<Vec<f32>> = vec![];
+        let mut res_in = vec![];
+        let mut res_out = vec![];
+        float_parsed.parse_elem(path)?;
+
+        for row in &float_parsed {
+            let age = row[0] / 100f32;
+            let rest_ecg = row[7] / 200f32;
+            let old_peak = row[9] / 5f32;
+            let heart_model = HeartModel::new(age, row[1], rest_ecg, old_peak, row[row.len()-1]);
+
+            res_in.push(heart_model.to_inp_out().0);
+            res_out.push(heart_model.to_inp_out().1);
+        }
+        
+        Ok((res_in, res_out))
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
+use std::error::Error;
+
+use heart_attack_dnn::heart_data::heart_model::HeartModel;
+use unda::core::data::input::Input;
+use unda::core::layer::methods::errors::ErrorTypes;
 use unda::core::network::Network;
 use unda::core::layer::{methods::activations::Activations,
 layers::{LayerTypes::DENSE, InputTypes}};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut heart_model = Network::new(4);
 
-    let input_size: u8 = 3;
+    let (inputs, outputs) = HeartModel::from_file("src/heart_data/data/heart.csv")?;
+    let mut inputs_dyn: Vec<&dyn Input> = vec![];
 
-    heart_model.set_input(InputTypes::DENSE(input_size as usize));
+    inputs.iter().for_each(|inp| {
+        inputs_dyn.push(inp);
+    });
 
+    heart_model.set_input(InputTypes::DENSE(inputs[0].len()));
+
+    heart_model.add_layer(DENSE(32, Activations::RELU, 0.01));
+    heart_model.add_layer(DENSE(64, Activations::RELU, 0.01));
     heart_model.add_layer(DENSE(1, Activations::SIGMOID, 0.01));
 
     heart_model.compile();
+
+    heart_model.fit(&inputs_dyn, &outputs, 10, ErrorTypes::CategoricalCrossEntropy);
+
+    Ok(())
 }
